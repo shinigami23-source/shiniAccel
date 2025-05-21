03b557ae01a1dc7e2a427a2875de11f9800de2d4
shinigami23-source <damionheath86@gmail.com>
2025-05-20 23:21:25 -0700

    Update basic.ino
    Added config defines from shiniConfig.h

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -1,6 +1,16 @@
 #define DEBUG 1  // Uncomment this line to enable debug
 #include <shiniAccel.h>
 
+#define MPU_SDA 32  //33
+#define MPU_SCL 33  //32
+#define OUTPUT_READABLE_ACCELGYRO
+#define GX_OFFSET -0.013323
+#define GY_OFFSET -0.114922
+#define GZ_OFFSET 0.006662
+#define AX_OFFSET 1.084573
+#define AY_OFFSET -0.024248
+#define AZ_OFFSET 10.56561
+
 shiniAccel mpu;
 
 void setup(){
