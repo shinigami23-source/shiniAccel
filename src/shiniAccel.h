3285091320208654dc8a0ca8e717753308a5f444
shinigami23-source <damionheath86@gmail.com>
2025-05-20 22:28:21 -0700

    Update shiniAccel.h

--- src/shiniAccel.h
+++ src/shiniAccel.h
@@ -1,1 +1,30 @@
 
+
+#ifndef shiniAccel_h
+#define shiniAccel_h
+#include <Wire.h>
+#include <I2Cdev.h>
+#include <MPU6050.h>
+#include <shiniConfig.h>
+ 
+
+   
+class shiniAccel{
+    private:
+
+    /* MPU6050 default I2C address is 0x68*/
+
+    int16_t ax, ay, az;
+    int16_t gx, gy, gz;
+    int8_t temp,lastTemp;
+
+    public:
+    void begin();
+    void read();
+
+
+};
+
+
+
+#endif
